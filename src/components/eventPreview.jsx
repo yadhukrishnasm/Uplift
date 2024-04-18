6ca28b804ad1c413facb7c6cfba480ee6f0cc00b
Savand Shyam Viswam <savandshyamviswam@gmail.com>
2024-04-18 09:41:53 +0530

    fix routing for first pages, correct some styles and add button to create new event

--- src/components/eventPreview.jsx
+++ src/components/eventPreview.jsx
@@ -1,13 +1,15 @@
-
-
 export default function EventPreview(props) {
   return (
-    <div className="fw-64 h-52 rounded-2xl relative shadow-[-4px_4px_0_0_rgba(56,163,165,1)]">
+    <div className="fw-64 h-52 rounded-2xl relative shadow-[-4px_4px_0_0_rgba(56,163,165,1)] w-full">
+      <div className="absolute right-0 px-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl border bg-lime-600 ">
+        slots {props.slots}
+      </div>
 
-      <div className="absolute right-0 px-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl border bg-lime-600 ">slots {props.slots}</div>
-
-      <img src= {props.img} alt="image"  className="h-1/2 border rounded-
-      tl-2xl rounded-tr-2xl " />
+      <img
+        src={props.img}
+        alt="image"
+        className="h-1/2 w-full border rounded-tl-2xl rounded-tr-2xl "
+      />
 
       <div className="p-2 flex-col flex gap-2">
         <h2 className="font-bold">{props.title}</h2>
@@ -15,5 +17,5 @@ export default function EventPreview(props) {
         <p className="font-semibold">{props.time}</p>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import WaterMark from "../components/WaterMark";
 
-export default function welcome() {
+export default function Welcome() {
+  const navigate = useNavigate();
   return (
     <div className="px-11 pt-40 flex flex-col py-20 gap-10 h-screen justify-between items-center font-poppins text-[#2D3136]">
       {/* <WaterMark /> */}
@@ -10,7 +12,10 @@ export default function welcome() {
         </h1>
         <p className="font-light">Together we can make a difference</p>
       </div>
-      <button className="bg-[#ACD293] shadow-[-1px_4px_0_0_rgba(0,0,0,.5)] text-black font-light py-[.4rem] px-6 rounded-full w-fit border border-black">
+      <button
+        className="bg-[#ACD293] shadow-[-1px_4px_0_0_rgba(0,0,0,.5)] text-black font-light py-[.4rem] px-6 rounded-full w-fit border border-black"
+        onClick={() => navigate("/signin")}
+      >
         sign up
       </button>
     </div>
